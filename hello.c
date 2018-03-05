b3a24c99fd50640413a50a9df2198d6e2bddf991
aryeshdb <aryeshdb@iitk.ac.in>
2018-03-05 15:29:03 +0530

    1-6

--- hello.c
+++ hello.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
-void microkernel_sendmsg(char *);
 void main(){
 printf("Helloworld!\n");
-
 printf("This must be a monolithic
 design\n");
-
 microkernel_sendmsg("is more
 portable");
 }
